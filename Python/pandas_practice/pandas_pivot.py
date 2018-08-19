bd240587c697398b93786deae7e34cf735b43b60
Scott Burklund <scott.burklund@preverity.com>
2018-08-19 12:43:22 -0500

    started working with Pandas pivot function in pandas_practice Modified spacing to be PEP8 compliant

---
+++ Python/pandas_practice/pandas_pivot.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import datetime
+
+df = pd.DataFrame({'A': ['one', 'one', 'two', 'three'] * 6,
+                   'B': ['A', 'B', 'C'] * 8,
+                   'C': ['foo', 'foo', 'foo', 'bar', 'bar', 'bar'] * 4,
+                   'D': np.random.randn(24),
+                   'E': np.random.randn(24),
+                   'F': [datetime.datetime(2013, i, 1) for i in range(1, 13)] +
+                        [datetime.datetime(2013, i, 15) for i in range(1, 13)]})
--- Python/python_programming_net/data_analysis_pandas_p3.py
+++ Python/python_programming_net/data_analysis_pandas_p3.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df3 = pd.read_csv('data/HPI_AT_state.csv', names= ['state', 'year', 'qtr', 'hpi'])
+df3 = pd.read_csv('data/HPI_AT_state.csv', names=['state', 'year', 'qtr', 'hpi'])
 print(df3.head())
 
 df3.to_html('data/hpi_at_state.html')
@@ -10,11 +10,12 @@ print(df3z.head())
 print(list(df3z))
 print(df3z.shape)
 print(df3z.columns)
-#print(df3z.dtypes)
+# print(df3z.dtypes)
 print(df3z.count())
 
 # reshape the data so the year month of the value is in a row instead of 274 columns
-df3zz = df3z.melt(id_vars=['RegionID','RegionName','City','State','Metro','CountyName','SizeRank'], var_name='year_month')
-#cheese.melt(id_vars=['first', 'last'], var_name='quantity')
+df3zz = df3z.melt(id_vars=['RegionID', 'RegionName', 'City', 'State', 'Metro', 'CountyName', 'SizeRank'],
+                  var_name='year_month')
+# cheese.melt(id_vars=['first', 'last'], var_name='quantity')
 print(df3zz.head(10))
 print(df3zz.tail(10))
