b88572013d202aef16b45cadcf312d73381935bc
Scott Burklund <scott.burklund@preverity.com>
2018-11-05 15:43:40 -0600

    finished prog 5

--- Python/python_programming_net/data_analysis_pandas_p5.py
+++ Python/python_programming_net/data_analysis_pandas_p5.py
@@ -14,4 +14,19 @@ df3 = pd.DataFrame({'HPI':[80,85,88,85],
                     'Int_rate':[2, 3, 2, 2],
                     'Low_tier_HPI':[50, 52, 50, 53]},
                    index = [2001, 2002, 2003, 2004])
-                   
+
+concat = pd.concat([df1, df2]) 
+print(concat)
+
+concat = pd.concat([df1, df2, df3]) 
+print(concat) 
+
+df4 = df1.append(df2) 
+print(df4) 
+
+df4 = df1.append(df3) 
+print(df4) 
+
+s = pd.Series([80,2,50], index=['HPI','Int_rate','US_GDP_Thousands']) 
+df4 = df1.append(s, ignore_index=True)
+print(df4)
