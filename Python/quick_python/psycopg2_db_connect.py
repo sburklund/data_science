a895f7670573de71381b68d1964a848cda1e7d52
Scott Burklund <scott.burklund@preverity.com>
2019-06-02 12:34:05 -0500

    working psycopg2_db_connect to Redshift

--- Python/quick_python/psycopg2_db_connect.py
+++ Python/quick_python/psycopg2_db_connect.py
@@ -1,16 +1,28 @@
 import psycopg2
-conn=psycopg2.connect(dbname= 'prod', 
-                     host='preverity-prod.c1klgawkvuni.us-east-1.redshift.amazonaws.com', 
-                     port= '5439', 
-                     user= 'username', 
-                     password= 'password'
-                     )
+
+# Try to connect
+try:   
+    conn=psycopg2.connect(dbname= 'prod', 
+                          host='preverity-prod.c1klgawkvuni.us-east-1.redshift.amazonaws.com', 
+                          port= '5439', 
+                          user= 'username', 
+                          password= 'password'
+                          )
+except:
+    print("I am unable to connect to the database.")
 
 cur = conn.cursor()  # create a cursor for executing queries
 cur.execute("select * from sburklund.nucc_compare;")
-cur.fetchall()
-print(cur.fetchall())
+data = cur.fetchall()
 
+for row in data:
+    print(row)
+
+cur.execute("select * from edw.claims_monthly where proc_month >= '20190301' order by proc_month, claim_type;")
+data = cur.fetchall()
+
+for row in data:
+    print(row)
 
 cur.close() 
 conn.close()
