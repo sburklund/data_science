059062a0c71b01849c19c89aa18d4bbbbb0bb55d
Scott Burklund <scott.burklund@preverity.com>
2019-06-24 05:48:47 -0500

    pandas_sqlalchemy_connect.py initial commit

---
+++ Python/quick_python/pandas_sqlalchemy_connect.py
@@ -0,0 +1,42 @@
+#
+#  Access your data in Amazon Redshift and PostgreSQL with Python and R
+#  https://www.blendo.co/blog/access-your-data-in-amazon-redshift-and-postgresql-with-python-and-r/?utm_medium=referral&utm_source=quora.com&utm_campaign=connect-to-redshift-using-python
+
+
+############ REQUIREMENTS ####################
+# sudo apt-get install python-pip 
+# sudo apt-get install libpq-dev
+# sudo pip install psycopg2
+# sudo pip install sqlalchemy
+# sudo pip install sqlalchemy-redshift
+# sudo pip install pandas
+##############################################
+
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+import psycopg2
+import os
+import pprint as pretty
+
+
+from sqlalchemy import create_engine
+import psycopg2
+import pandas as padas
+import os
+
+redshift_endpoint = os.getenv("REDSHIFT_ENDPOINT")
+redshift_user = os.getenv("REDSHIFT_USER")
+redshift_pass = os.getenv("REDSHIFT_PASS")
+port = 5439
+dbname = 'prod'
+
+engine = create_engine('postgresql://redshift_user:redshift_pass@redshift_endpoint:port/dbname')
+data_frame = padas.read_sql_query('SELECT * FROM sburklund.nucc_compare;', engine)
+
+
+engine = create_engine('postgresql://scott:tiger@hredshift_host:5439/mydatabase')
+data_frame = padas.read_sql_query('SELECT * FROM `table`;', engine)
+
+
+engine = create_engine('postgresql://redshift_user:redshift_pass@redshift_endpoint:port/dbname')
+data_frame = padas.read_sql_query('SELECT * FROM sburklund.nucc_compare;', engine)
