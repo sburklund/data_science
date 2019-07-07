2eecefef5b417fd693989862584861a9440348ab
Scott Burklund <scott.burklund@preverity.com>
2019-07-07 12:02:44 -0500

    Test IPYNB import in vscode and convert to python

---
+++ Python/quick_python/ipynb_imported_to_python_vscode.py
@@ -0,0 +1,54 @@
+#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+# ms-python.python added
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'Python\\quick_python'))
+	print(os.getcwd())
+except:
+	pass
+#%% [markdown]
+# ### Import libraries and set up Redshift connnection string
+
+#%%
+from sqlalchemy import create_engine
+import psycopg2
+import pandas as pd
+import os
+
+
+#%%
+redshift_endpoint = os.getenv("REDSHIFT_ENDPOINT")
+redshift_user = os.getenv("REDSHIFT_USER")
+redshift_pass = os.getenv("REDSHIFT_PASS")
+port = 5439
+dbname = 'prod'
+
+engine_str = "redshift+psycopg2://%s:%s@%s:%s/%s" % (redshift_user,redshift_pass,redshift_endpoint,str(port),dbname)
+
+engine = create_engine(engine_str)
+
+#%% [markdown]
+# ### Pull sample data from Redshift into a Pandas dataframe
+# show the first 5 rows of the data
+
+#%%
+mx_data = pd.read_sql_query('select * from edw.mhx_claims limit 50;', engine)
+mx_data.head()
+
+#%% [markdown]
+# ### Group data by payer_plan_type with counts of rows
+
+#%%
+mx_data.groupby(['payer_plan_type']).count()
+
+#%% [markdown]
+# ### Group data by payer_plan_type with counts of rows on 1 column
+
+#%%
+mx_data[['payer_plan_type', 'claim_txn_id']].groupby(['payer_plan_type']).count()
+
+
+#%%
+
+
+
